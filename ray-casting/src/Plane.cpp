6c276ed756be10722068aaad8c8fbc81704b6956
izaak <izaakniksan@gmail.com>
2020-07-19 17:01:51 -0400

    Implement plane intersection

--- ray-casting/src/Plane.cpp
+++ ray-casting/src/Plane.cpp
@@ -4,9 +4,20 @@
 bool Plane::intersect(
   const Ray & ray, const double min_t, double & t, Eigen::Vector3d & n) const
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  return false;
-  ////////////////////////////////////////////////////////////////////////////
+
+  if (ray.direction.dot(this->normal) == 0){
+    return false;
+  }
+  // plane/line intersection equation:
+  // (p - p0) dot n = 0
+  // (e + td - p0) dot n = 0
+
+  double temp_t = ((this->normal).dot(this->point - ray.origin))/((ray.direction).dot(this->normal));
+  if (temp_t > min_t){
+    t = temp_t;
+    n = this->normal;
+    return true;
+  }
+  return false; 
 }
 
