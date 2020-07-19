d5d1a51cef0b0cec116f1e2285331b99bd4e3b26
izaak <izaakniksan@gmail.com>
2020-07-19 08:31:42 -0400

    Rays intersect with first sphere

--- ray-casting/src/first_hit.cpp
+++ ray-casting/src/first_hit.cpp
@@ -1,4 +1,5 @@
 #include "first_hit.h"
+#include <iostream>
 
 bool first_hit(
   const Ray & ray, 
@@ -8,9 +9,20 @@ bool first_hit(
   double & t,
   Eigen::Vector3d & n)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  return false;
-  ////////////////////////////////////////////////////////////////////////////
+  bool does_hit = false;
+  Eigen::Vector3d temp_n;
+  double temp_t; 
+  for(int i=0; i<objects.size();++i){
+    auto x=objects[i];
+    if ((*x).intersect(ray, min_t, temp_t, temp_n)){
+      if (!does_hit || temp_t < t){
+        does_hit = true;
+        t=temp_t;
+        n=temp_n;
+        hit_id=i;
+      }
+    }
+  };
+  return does_hit;
 }
 
